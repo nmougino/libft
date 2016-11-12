d5d624eeccbd21fef96fbda637ce4083429fe7c5
nmougino <nmougino@student.42.fr>
2016-11-12 17:58:21 +0100

    Norme

--- srcs/string/ft_strtrim.c
+++ srcs/string/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 09:36:41 by nmougino          #+#    #+#             */
-/*   Updated: 2016/10/20 21:25:32 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/11/12 17:54:33 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,20 @@
 ** nettoyee des caracteres blanc ' ', '\t', '\n' en debut et fin de chaine.
 */
 
-char	*ft_strtrim(char const *s)
+char	*ft_strtrim(const char *s)
 {
-	unsigned long	q;
-	unsigned long	o;
+	int		i;
+	char	*ret;
 
-	o = 0;
-	q = 0;
-	if (s != NULL && s[0] != '\0')
-	{
-		q = ft_strlen(s) - 1;
-		while (s[o] == ' ' || s[o] == '\t' || s[o] == '\n')
-			o++;
-		if (s[o + 1] == '\0')
-			return (NULL);
-		while ((s[q] == ' ' || s[q] == '\t' || s[q] == '\n') && q > 0)
-			q--;
-		if (q == 0)
-			return (ft_strdup(""));
-		if (o < q)
-			return (ft_strsub(s, (unsigned int)o, (q + 1 - o)));
-	}
-	if (s[0] == '\0')
-		return (ft_strdup(""));
-	return (NULL);
+	if (!s)
+		return (NULL);
+	while (*s == ' ' || *s == '\n' || *s == '\t')
+		++s;
+	i = ft_strlen(s) - 1;
+	while (i >= 0 && (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'))
+		--i;
+	++i;
+	ret = ft_strnew(i);
+	ft_strncpy(ret, s, i);
+	return (ret);
 }
