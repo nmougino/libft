f6eb8a66566d510614d5681f5ddb44f8b45d445d
Akasha <nmougino@student.42.fr>
2017-04-30 19:47:15 +0200

    Attempt a fix of arrstrncpy

--- srcs/array/ft_arrstrncpy.c
+++ srcs/array/ft_arrstrncpy.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/30 18:50:37 by nmougino          #+#    #+#             */
-/*   Updated: 2017/04/30 19:38:02 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/04/30 19:46:52 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	**ft_arrstrncpy(char **src, size_t n)
 		tab[i] = ft_strdup(src[i]);
 		++i;
 	}
-	while (i < n)
+	while (i <= n)
 	{
 		tab[i] = NULL;
 		++i;
