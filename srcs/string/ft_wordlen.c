0cfdb7e3a0edb8a7c7bd8c31582c70f464cf0193
Akasha <nmougino@student.42.fr>
2017-07-12 20:38:59 +0200

    Change wordlen

--- srcs/string/ft_wordlen.c
+++ srcs/string/ft_wordlen.c
@@ -6,18 +6,18 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 00:18:36 by nmougino          #+#    #+#             */
-/*   Updated: 2017/07/12 00:20:45 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/07/12 20:38:27 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_wordlen(char *str)
+size_t	ft_wordlen(char *str, char *seps)
 {
 	size_t	i;
 
 	i = 0;
-	while (str[i] && !ft_strchr(" \n\t", str[i]))
+	while (str[i] && !ft_strchr(seps, str[i]))
 		++i;
 	return (i);
 }
