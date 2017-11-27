ac103a8591c977a742349a0f3c1f6e72b75f3069
Akasha <nmougino@student.42.fr>
2017-11-27 15:57:07 +0100

    Fix little thing on strchr

--- srcs/string/ft_strchr.c
+++ srcs/string/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 05:18:04 by nmougino          #+#    #+#             */
-/*   Updated: 2017/07/31 16:56:46 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/11/27 15:56:23 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] && (s[i] != (char)c))
 		i++;
-	if (s[i] && s[i] == (char)c)
+	if (s[i] == (char)c)
 		return (char *)((unsigned long)s + i);
 	return (NULL);
 }
