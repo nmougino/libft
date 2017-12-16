c996703aee10c86b872e1d2b5769fc005defc51c
Akasha <nmougino@student.42.fr>
2017-12-16 18:41:59 +0100

    Put a function in inline

--- srcs/printf/addto.c
+++ srcs/printf/addto.c
@@ -6,13 +6,13 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:05:45 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/19 16:50:09 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/12/16 18:40:44 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	addto(char c, t_print *print)
+inline void	addto(char c, t_print *print)
 {
 	if (print->pos == BUF_SIZE)
 	{
